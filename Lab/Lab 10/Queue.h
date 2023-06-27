37cda396ec2f12bcf282bf11d1a7f0e2df83f541
LostManJayK <andrews2192@hotmail.com>
2023-06-27 15:09:50 -0700

    Lab 10 for submission

--- Lab/Lab 10/Queue.h
+++ Lab/Lab 10/Queue.h
@@ -11,25 +11,8 @@ private:
 	int numItems;
 public:
 
-	template <class T>
-	Queue<T>(int s)
-	{
-		queueArray = new T[s];
-		capacity = s;
-		numItems = 0;
-	};
-
-	template <class T>
-	Queue<T>::Queue(Queue<T>& q)
-	{
-		this->capacity = q.capacity;
-		this->numItems = q.numItems;
-		this->queueArray = new T[this->capacity];
-		for (int i = 0; i < this->numItems; i++)
-		{
-			this->queueArray[i] = q.queueArray[i];
-		}
-	};
+	Queue(int);
+	Queue(Queue<T>&);
 
 	~Queue();
 	void enqueue(T);
